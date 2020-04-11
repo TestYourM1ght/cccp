b80cd671b6ba565cbdbdb4af4b3aea8e4f538176
TestYourM1ght <vera_v_uspeh@list.ru>
2020-04-11 23:49:57 +0300

    добавили form-controll, красивую кнопку

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root 'posts#index'
+  root 'posts#index', as: 'home'
 
-  get 'about' => 'pages#about'
+  get 'about' => 'pages#about', as: 'about'
+  get 'donations' => 'pages#donations', as: 'donations'
 resources :posts
 
 end
